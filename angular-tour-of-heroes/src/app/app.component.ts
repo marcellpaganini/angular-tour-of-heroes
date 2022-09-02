1b96a2d474329e19d2e4bdac90ae6900fbc77f10
Marcell Tanure <59565705+marcellpaganini@users.noreply.github.com>
2022-09-02 11:47:01 -0300

    class style and keydown event  binding

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+  isUgly = false;
+
+  uglySwitch() {
+    this.isUgly = !this.isUgly;
+  }
 }
